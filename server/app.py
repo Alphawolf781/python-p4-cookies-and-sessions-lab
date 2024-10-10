5420899ff3328f64b4e804b6a6f048a0ab2a63ed
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-10-10 09:53:57 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,25 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = Article.query.all()
+    return jsonify(
+        [article.to_dict() for article in articles]
+    )
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views']= session.get('page_views', 0) + 1
+
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}),401
+    
+    article = Article.query.get(id)
+    if article is None:
+        return ({'message':'Article not found'}),404
+    
+    return jsonify(article.to_dict())
+
 
 if __name__ == '__main__':
     app.run(port=5555)
